d7eccf66ac55774b093306cf634ebb7b669289db
bbtechpro <billy.barber@gmail.com>
2026-05-06 21:13:26 -0400

    Was missing the export default line at the end of this file, which was probablt the root of all the tailwidncss errors I was having.

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,27 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import RecipeDetail from './pages/RecipeDetail';
-import Favorites from './pages/Favorites.jsx';
+import Favorites from './pages/Favorites';
+import CategoryMeals from './pages/CategoryMeals';
+import SearchResults from './pages/SearchResults';
 import { FavProvider } from './components/FavContext';
 import './App.css'
-import Header from './components/Header.jsx';
+import Header from './components/Header';
 
 function App() {
   return (
     <FavProvider>
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/recipe/:id" element={<RecipeDetail />} />
-        <Route path="/favorites" element={<Favorites />} />
-      </Routes>
-    </Router>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/recipe/:id" element={<RecipeDetail />} />
+          <Route path="/category/:categoryName" element={<CategoryMeals />} />
+          <Route path="/search" element={<SearchResults />} />
+          <Route path="/favorites" element={<Favorites />} />
+        </Routes>
+      </Router>
     </FavProvider>
   );
 }
+export default App;
