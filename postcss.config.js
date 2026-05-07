0b38f1d31e4b457a5b688b1fb186be7ad4cde0b3
bbtechpro <billy.barber@gmail.com>
2026-05-06 21:12:22 -0400

    It took several attempts to get rid of console errors when running npm run dev to test the app.

---
+++ postcss.config.js
@@ -0,0 +1,22 @@
+// export default {
+//   plugins: {
+//     tailwindcss: {},
+//     autoprefixer: {},
+//   },
+// }
+
+// postcss.config.js
+// module.exports = {
+//   plugins: {
+//     '@tailwindcss/postcss': {}, // Use this instead of 'tailwindcss'
+//     autoprefixer: {},
+//   },
+// }
+
+
+export default {
+  plugins: {
+    '@tailwindcss/postcss': {},
+    autoprefixer: {},
+  },
+};
